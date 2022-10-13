ebab0d189ac6c6d2abcd16d3dd2889a152b867e4
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-13 14:01:51 -0500

    Add functional sidebar menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
