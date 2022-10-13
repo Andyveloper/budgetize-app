c16f53b18da96676d2b5f157d8855ab58cece88d
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-13 11:57:25 -0500

    Add redirection after signing in and devise path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || groups_url
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :groups
-  resources :expenditures
-  resources :users
+  devise_for :users, path: '/users', class_name: 'User'
+  authenticate :user do
+    resources :groups
+    resources :expenditures
+    resources :users
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
