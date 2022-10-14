dffb166a33656be331715e60131b794b8488d3a1
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-14 14:17:28 -0500

    Create join table and its respective model

--- db/migrate/20221013223957_create_join_table_group_expenditure.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableGroupExpenditure < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :groups, :expenditures do |t|
-      # t.index [:group_id, :expenditure_id]
-      # t.index [:expenditure_id, :group_id]
-    end
-  end
-end
