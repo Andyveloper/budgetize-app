4a29731154088d326465795edd27c51238141f45
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-14 14:20:08 -0500

    Add multiple selector to form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,4 +7,7 @@ Turbo.session.drive = false
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
+
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
 });
