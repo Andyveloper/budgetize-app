48521de200ab305e38d37d9ad9a8bd3c4c5dc775
Andyveloper <mcantillo.gdi@gmail.com>
2022-10-14 19:05:34 -0500

    Add specs for group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    before do
+      @category = Group.create(
+        name: 'Savings',
+        icon: 'icon1',
+        user_id: 1
+      )
+    end
+
+    context 'confirm the category name' do
+      it 'not exist' do
+        expect(@category.name).to eq(@category.name)
+      end
+    end
+  end
 end
